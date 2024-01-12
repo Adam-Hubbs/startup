55422503c44ffe7ad7a2eda18f0adf369d1f496c
Adam-Hubbs <amhubbs17@gmail.com>
2024-01-12 13:36:39 -0700

    Navigation Functionality on Person Element

---
+++ Person Element.js
@@ -0,0 +1,23 @@
+function showSection(section) {
+  let sections = [
+    "TimelineContent",
+    "TasksContent",
+    "GoalsContent",
+    "NotesContent",
+    "FilesContent",
+  ];
+
+  // Hide all sections
+  sections.forEach(function (s) {
+    let element = document.querySelector("." + s);
+    if (element) {
+      element.style.display = "none";
+    }
+  });
+
+  // Show the selected section
+  let selectedElement = document.querySelector("." + section);
+  if (selectedElement) {
+    selectedElement.style.display = "block";
+  }
+}
