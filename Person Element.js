4672d1e0bec732a025a5efd60dd5e580a60c6e6b
Adam-Hubbs <amhubbs17@gmail.com>
2024-02-02 14:58:01 -0700

    Login first try

--- Person Element.js
+++ Person Element.js
@@ -1,23 +1,30 @@
 function showSection(section) {
-  let sections = [
-    "TimelineContent",
-    "TasksContent",
-    "GoalsContent",
-    "NotesContent",
-    "FilesContent",
-  ];
+    let sections = [
+        "TimelineContent",
+        "TasksContent",
+        "GoalsContent",
+        "NotesContent",
+        "FilesContent",
+    ];
 
-  // Hide all sections
-  sections.forEach(function (s) {
-    let element = document.querySelector("." + s);
-    if (element) {
-      element.style.display = "none";
+    // Hide all sections
+    sections.forEach(function (s) {
+        let element = document.querySelector("." + s);
+        if (element) {
+            element.style.display = "none";
+        }
+    });
+
+    // Show the selected section
+    let selectedElement = document.querySelector("." + section);
+    if (selectedElement) {
+        selectedElement.style.display = "block";
     }
-  });
-
-  // Show the selected section
-  let selectedElement = document.querySelector("." + section);
-  if (selectedElement) {
-    selectedElement.style.display = "block";
-  }
 }
+
+
+
+
+
+
+
